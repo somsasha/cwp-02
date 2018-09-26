4dced7a5e9e448bfb679d0d9ff6ab9eb275880aa
somsasha <somsasha111@gmail.com>
2018-09-26 09:31:59 +0300

    questions-client

--- client.js
+++ client.js
@@ -1,18 +1,45 @@
 const net = require('net');
 const port = 8124;
+message = process.argv[2];
 
 const client = new net.Socket();
+var answersArray = [];
+var questionArray = [];
+var questionCounter = 0;
 
 client.setEncoding('utf8');
 
 client.connect(port, function() {
-  console.log('Connected');
-  client.write('\r\nHello, Server!\r\nLove,\r\nClient.\r\n');
+    client.write("QA");
+    console.log('Connected');
+    client.write('\r\nHello, Server!\r\nLove,\r\nClient.\r\n');
 });
 
 client.on('data', function(data) {
   console.log(data);
-  client.destroy();
+  if (data === "ACK") {
+        var qaJson = require('./qa.json');
+        answersArray = [qaJson.Q1, qaJson.Q2, qaJson.Q3];
+        questionArray = ["Q1", "Q2", "Q3"];
+        console.log("Question: " + questionArray[questionCounter]);
+        client.write(questionArray[questionCounter]);
+        questionCounter++;
+    }
+    if (data !== "ACK" && data !== "DEC" && data.startsWith("A") && questionCounter <= questionArray.length) {
+        if (data === answersArray[questionCounter - 1]) {
+            console.log("Correct :)");
+        }
+        else {
+            console.log("Wrong :(")
+        }
+        if (questionCounter != questionArray.length)
+        {
+            console.log("\r\nQuestion: " + questionArray[questionCounter]);
+            client.write(questionArray[questionCounter]);
+            questionCounter++;
+        }
+    }
+  //client.destroy();
 });
 
 client.on('close', function() {
--- server.js
+++ server.js
@@ -3,19 +3,25 @@ const port = 8124;
 
 let clients = [];
 let seed = 0;
-const server = net.createServer((client) => {
-
+const server = net.createServer((connection) => {
+    connection.on('data', (data) => {
+        console.log('client message: ' + data);
+        if (data=='QA')
+            connection.write('ACK');
+        else
+            { connection.write('DEC');    }
+      });
     clients[clients.length] = Date.now() + seed++;
-  console.log('Client connected, id: ' + clients[clients.length-1]);
+    console.log('Client connected, id: ' + clients[clients.length-1]);
 
-  client.setEncoding('utf8');
+    connection.setEncoding('utf8');
 
-  client.on('data', (data) => {
-    console.log(data);
-    client.write('\r\nHello!\r\nRegards,\r\nServer\r\n');
-  });
+//     connection.on('data', (data) => {
+//     console.log(data);
+//     connection.write('\r\nHello!\r\nRegards,\r\nServer\r\n');
+//   });
 
-  client.on('end', () => console.log('Client disconnected'));
+  connection.on('end', () => console.log('Client disconnected'));
 });
 
 server.listen(port, () => {
