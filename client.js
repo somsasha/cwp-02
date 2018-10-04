69399e303dbb9762fe6d0fbd5836d5af836556db
somsasha <somsasha111@gmail.com>
2018-10-04 08:41:23 +0300

    BugFix

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 const net = require('net');
 const port = 8124;
-message = process.argv[2];
 
 const client = new net.Socket();
 var answersArray = [];
@@ -10,16 +9,15 @@ var questionCounter = 0;
 client.setEncoding('utf8');
 
 client.connect(port, function() {
-    client.write("QA");
     console.log('Connected');
-    client.write('\r\nHello, Server!\r\nLove,\r\nClient.\r\n');
+    client.write("QA");
 });
 
 client.on('data', function(data) {
   console.log(data);
   if (data === "ACK") {
-        var qaJson = require('./qa.json');
-        answersArray = [qaJson.Q1, qaJson.Q2, qaJson.Q3];
+        var qa = require('./qa.json');
+        answersArray = [qa.Q1, qa.Q2, qa.Q3];
         questionArray = ["Q1", "Q2", "Q3"];
         console.log("Question: " + questionArray[questionCounter]);
         client.write(questionArray[questionCounter]);
@@ -27,10 +25,10 @@ client.on('data', function(data) {
     }
     if (data !== "ACK" && data !== "DEC" && data.startsWith("A") && questionCounter <= questionArray.length) {
         if (data === answersArray[questionCounter - 1]) {
-            console.log("Correct :)");
+            console.log("Correct");
         }
         else {
-            console.log("Wrong :(")
+            console.log("Wrong")
         }
         if (questionCounter != questionArray.length)
         {
@@ -38,8 +36,10 @@ client.on('data', function(data) {
             client.write(questionArray[questionCounter]);
             questionCounter++;
         }
+        else{
+            client.destroy();
+        }
     }
-  //client.destroy();
 });
 
 client.on('close', function() {
